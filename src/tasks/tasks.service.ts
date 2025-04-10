d7024e922e9b045cd24cad36782756c54e676228
Aayush Dhungel <adhungel0413@gmail.com>
2025-04-11 00:14:43 +0545

    Add CreateTaskDto and update tasks controller and service to use DTO for task creation

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -12,7 +13,13 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  getTaskById(id: string): Task | undefined {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
     const task: Task = {
       id: uuid(),
       title,
