f43e006af591b1a33a3e1441903f92a2ef5a74fe
Aayush Dhungel <adhungel0413@gmail.com>
2025-04-12 10:50:47 +0545

    Feature: Searching and Filtering

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -11,6 +12,28 @@ export class TasksService {
   // methods
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+
+    let tasks = this.getAllTasks();
+
+    if (status) {
+      tasks = tasks.filter((task) => task.status === status);
+    }
+
+    if (search) {
+      const lowerCaseSearch = search.toLowerCase();
+
+      tasks = tasks.filter(
+        (task) =>
+          task.title.toLowerCase().includes(lowerCaseSearch) ||
+          task.description.toLowerCase().includes(lowerCaseSearch),
+      );
+    }
+
+    return tasks;
   }
 
   getTaskById(id: string): Task | undefined {
